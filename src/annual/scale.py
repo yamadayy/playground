c0d7a98b4ecda333417bda4580b26bc0bd4855f3
yamada <yamada.yoshiyuki@gmail.com>
2023-04-26 17:49:14 +0900

    Add new python files.

---
+++ src/annual/scale.py
@@ -0,0 +1,4 @@
+class Scale:
+    def __init__(self, o: float, s: float):
+        self.__origin = o
+        self.__scale = s
---
+++ src/annual/star.py
@@ -0,0 +1,3 @@
+class Star:
+    def __init__(self, x: float):
+        self.__position = x
