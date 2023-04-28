d0496f783348ec501df824bc0f0a8ceb69235b36
yamada <yamada.yoshiyuki@gmail.com>
2023-04-28 17:54:41 +0900

    Fix parameters.

--- src/annual/client.py
+++ src/annual/client.py
@@ -7,16 +7,14 @@ if __name__ == '__main__':
     num_star = 10
     num_reference_star = 10
     line_scale = 100
-    amplitude_annual_scale = 0.08
+    amplitude_annual_scale = 0.25
     amplitude_every_observation = 0.01
-    num_exposure = 10
+    num_exposure_per_season = 50
+    observation_accuracy = 1
+    reference_accuracy = 0.1
     target_accuracy = 0.03
     stars = []
     for i in range(num_star):
         x = random.random() * line_scale
         print(x)
         stars.append(Star(x))
-
-    print()
-    for i in range(len(stars)):
-        print(stars[i].position())
