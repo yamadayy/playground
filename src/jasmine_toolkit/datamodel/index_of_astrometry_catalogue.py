2dba2964ce0f9bdb7440ece6e38c44dc6db67c64
yamada <yamada.yoshiyuki@gmail.com>
2023-07-19 16:35:02 +0900

    Add enum of array index.

---
+++ src/jasmine_toolkit/datamodel/index_of_astrometry_catalogue.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class EnumAstrometry(Enum):
+    STELLAR_ID = 0
+    LONGITUDE = 1
+    LATITUDE = 2
+    MAGNITUDE = 3
+    DATETIME = 4
+    PM_LONGITUDE = 5
+    PM_LATITUDE = 6
+    PARALLAX = 7
